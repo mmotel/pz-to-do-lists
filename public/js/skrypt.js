46dbe58608e8c08d87165649062af1f3a220bd5c
Mateusz Motel <mateuszmotel@gmail.com>
2013-10-13 20:47:06 +0200

    created structure for loggedin and not loggedin GUIs

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -1,12 +1,37 @@
 /*jshint node: true, browser: true, jquery: true */
 /*global io: false */
 $(document).ready(function () {
-    'use strict';
-    var socket = io.connect('http://localhost:3000');
+	'use strict';
 
-    console.log('connecting…');
+	$.getJSON('http://localhost:3000/getLogin', 
+	function(data){ 
+		console.log("data [ " + data + " ]"); 
+		if(data !== null) {
+			console.log('####@@@@loggedin!');
+			//actions if user is loggedin
+			loggedinCallback(data); 
+		}
+		else {
+			console.log('%%%%&&&&NOT loggedin!');
+			//actions if user is not loggedin
+			loginCallback();
+		}
+	});
 
-    socket.on('connect', function () {
-        console.log('Połączony!');
-    });
+	//actions if user is loggedin
+	var loggedinCallback = function (data){
+
+		var socket = io.connect('http://localhost:3000');
+
+		console.log('connecting…');
+
+		socket.on('connect', function () {
+			console.log('Połączony!');
+		});
+	};
+
+	//actions if user is not loggedin
+	var loginCallback = function (){
+		//show content to login with fb
+	};
 });
