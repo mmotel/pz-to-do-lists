2542ab478b402486d2e43330a1aea0d2a78541f0
Flover <emflover@gmail.com>
2013-12-14 00:37:23 +0100

    added test for dataManager lib findTask method

--- test/taskManagerTests.js
+++ test/taskManagerTests.js
@@ -32,3 +32,47 @@ var Task = function(fbid, listid, name, descr, deadline){
   		});
   	});
   });
+
+  describe('Manager.findTask', function (){
+  	it('should find task on list if exist', function (done){
+  		var newTask = new Task(1, 1, 'moje zadanie', 'moj opis', new Date(2014,10,15,14,0,0,0));
+  		Manager.addTask(newTask, function (err, item){
+  			if (err){ done(err); }
+  			else{
+  				assert.notStrictEqual(undefined, item._id);
+				assert.notStrictEqual(undefined, item.id);
+				assert.strictEqual(newTask.fbid, item.fbid);
+				assert.strictEqual(newTask.listid, item.listid);
+				assert.strictEqual(newTask.name, item.name);
+				assert.strictEqual(newTask.descr, item.descr);
+				assert.strictEqual(newTask.deadline, item.deadline);
+
+				Manager.findTask(item.id, function (err, item2){
+					if (err){ done(err); }
+					else{
+						assert.strictEqual(item._id.toString(), item2._id.toString());
+						assert.strictEqual(item.id, item2.id);
+						assert.strictEqual(item.fbid, item2.fbid);
+						assert.strictEqual(item.listid, item2.listid);
+						assert.strictEqual(item.name, item2.name);
+						assert.strictEqual(item.descr, item2.descr);
+						assert.strictEqual(item.deadline.toString(), item2.deadline.toString());
+						assert.strictEqual(item.status, item2.status);
+						assert.strictEqual(item.trash, item2.trash);
+						done();
+					}
+				});
+  			}
+  		});
+  	});
+
+	it('should not find task if does not exist', function (done){
+		Manager.findTask(0, function (err, item){
+			if (err){ done(err); }
+			else{
+				assert.strictEqual(null, item);
+				done();
+			}
+		});
+	});
+  });
