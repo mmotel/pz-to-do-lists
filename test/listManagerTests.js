ae4a33e52f6dcba1b0ad0d3d51987a668f17162c
mmotel <mateuszmotel@gmail.com>
2013-12-13 17:42:16 +0100

    fixed test for lists & added fixed test do findAllLists

--- test/listManagerTests.js
+++ test/listManagerTests.js
@@ -3,8 +3,7 @@ var assert = require("assert"); //module used by tests
 var Data = require('../lib/newData.js')("test");
 var Manager = require('../lib/dataManager.js')(Data);
 
-var List = function(id, fbid, name, descr, trash){
-    this.id = id;
+var List = function(fbid, name, descr, trash){
     this.fbid = fbid;
     this.name = name;
     this.descr = descr;
@@ -12,20 +11,14 @@ var List = function(id, fbid, name, descr, trash){
     return this;
 };
 
-var genId = function(){
-    var t = new Date();
-    var id = t.getTime();
-    return id;
-  };
-
 describe('Manager.addList', function(){
 	it('should add new list', function (done){
-		newList = new List(genId(), 1, 'Moja Lista', 'Moj opis');
+		var newList = new List(1, 'Moja Lista', 'Moj opis');
 		Manager.addList(newList, function (err, item){
 			if (err){ done(err); }
 			else{
 				assert.notStrictEqual(undefined, item._id);
-				assert.strictEqual(newList.id, item.id);
+				assert.notStrictEqual(undefined, item.id);
 				assert.strictEqual(newList.fbid, item.fbid);
 				assert.strictEqual(newList.name, item.name);
 				assert.strictEqual(newList.descr, item.descr);
@@ -37,12 +30,12 @@ describe('Manager.addList', function(){
 
 describe('Manager.findList', function(){
 	it('should find list if exist', function (done){
-		newList = new List(genId(), 1, 'Moja Lista', 'Moj opis');
+		var newList = new List(1, 'Moja Lista', 'Moj opis');
 		Manager.addList(newList, function (err, item){
 			if (err){ done(err); }
 			else{
 				assert.notStrictEqual(undefined, item._id);
-				assert.strictEqual(newList.id, item.id);
+				assert.notStrictEqual(undefined, item.id);
 				assert.strictEqual(newList.fbid, item.fbid);
 				assert.strictEqual(newList.name, item.name);
 				assert.strictEqual(newList.descr, item.descr);
@@ -72,5 +65,67 @@ describe('Manager.findList', function(){
 				done();
 			}
 		})
+	});
+});
+
+describe('Manager.findAllLists', function (){
+	it('should find user all lists', function (done){
+		var newList = new List(2, 'Moja Lista', 'Moj opis');
+		Manager.addList(newList, function (err, item){
+			if (err){ done(); }
+			else{
+				assert.notStrictEqual(undefined, item._id);
+				assert.notStrictEqual(undefined, item.id);
+				assert.strictEqual(newList.fbid, item.fbid);
+				assert.strictEqual(newList.name, item.name);
+				assert.strictEqual(newList.descr, item.descr);
+				assert.strictEqual(newList.trash, item.trash);
+
+				var newList2 = new List(2, 'Moja Lista2', 'Moj opis2');
+				Manager.addList(newList2, function (err, item2){
+					if (err) { done(); }
+					else{
+						assert.notStrictEqual(undefined, item2._id);
+						assert.notStrictEqual(undefined, item2.id);
+						assert.strictEqual(newList2.fbid, item2.fbid);
+						assert.strictEqual(newList2.name, item2.name);
+						assert.strictEqual(newList2.descr, item2.descr);
+						assert.strictEqual(newList2.trash, item2.trash);
+						
+						Manager.findAllLists(2, function (err, items){
+							if (err){ done(); }
+							else{
+								assert.strictEqual(2, items.length);
+								items.sort(function (a, b){
+									if (a.id < b.id){
+										return -1;
+									}
+									else if(a.id === b.id){
+										return 0; 
+									}
+									else {
+										return 1;
+									}
+								});
+								assert.strictEqual(items[0]._id.toString(), item._id.toString());
+								assert.strictEqual(items[0].id, item.id);
+								assert.strictEqual(items[0].fbid, item.fbid);
+								assert.strictEqual(items[0].name, item.name);
+								assert.strictEqual(items[0].descr, item.descr);
+								assert.strictEqual(items[0].trash, item.trash);
+
+								assert.strictEqual(items[1]._id.toString(), item2._id.toString());
+								assert.strictEqual(items[1].id, item2.id);
+								assert.strictEqual(items[1].fbid, item2.fbid);
+								assert.strictEqual(items[1].name, item2.name);
+								assert.strictEqual(items[1].descr, item2.descr);
+								assert.strictEqual(items[1].trash, item2.trash);
+								done();
+							}
+						});
+					}
+				});
+			}
+		});
 	});
 });
