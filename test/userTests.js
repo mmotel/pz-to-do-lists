41bdad37370b534d4091a19c2171b26f76b6eef1
mmotel <mateuszmotel@gmail.com>
2013-12-17 22:53:11 +0100

    updated tests for User lib login & logout methods

--- test/userTests.js
+++ test/userTests.js
@@ -1,11 +1,8 @@
 var assert = require("assert"); //module used by tests
 
-var appData = require('../lib/data.js')('test');
-var appUser = require('../lib/user.js')(appData);
-
-// console.log(appData);
-// console.log(appUser);
-
+var Data = require('../lib/newData.js')('toDoLists');
+var Manager = require('../lib/dataManager.js')(Data);
+var appUser = require('../lib/user.js')(Manager);
 
 var profile = function(id, displayName, familyName, givenName, middleName){
     this.id = id;
@@ -22,10 +19,8 @@ describe('User.login', function (){
 
     var demoProfile = profile(1, 'Testowy Profil', "Profil", "Test", "Testowy");
 
-    appUser.login(1, demoProfile, function (user) {
-      if(user === undefined){
-        throw err;
-      }
+    appUser.login(1, demoProfile, function (err, user) {
+      if(err){ done(err) ;}
       else{
         // console.log(user);
         assert.notStrictEqual(undefined, user._id);
@@ -46,10 +41,8 @@ describe('User.logout', function (){
 
     var demoProfile = profile(2, 'Testowy Profil', "Profil", "Test", "Testowy");
 
-    appUser.login(2, demoProfile, function (user){
-      if(user === undefined){
-        throw err;
-      }
+    appUser.login(2, demoProfile, function (err, user){
+      if(err){ done(err); }
       else{
         // console.log(user);
         assert.notStrictEqual(undefined, user._id);
