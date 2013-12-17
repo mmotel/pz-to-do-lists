ab82359d67596077d492276c9490d2f17795233e
mmotel <mateuszmotel@gmail.com>
2013-12-17 23:26:23 +0100

    added tests for user lib removeAccount method

--- test/userTests.js
+++ test/userTests.js
@@ -1,6 +1,6 @@
 var assert = require("assert"); //module used by tests
 
-var Data = require('../lib/newData.js')('toDoLists');
+var Data = require('../lib/newData.js')('test');
 var Manager = require('../lib/dataManager.js')(Data);
 var appUser = require('../lib/user.js')(Manager);
 
@@ -60,7 +60,114 @@ describe('User.logout', function (){
   });
   it('should return null when user is notloggedin', function (){
 
-    assert.strictEqual(null, appUser.logout(3));
+    assert.strictEqual(null, appUser.logout(0));
+
+  });
+});
+
+describe('User.getLogin', function (){
+  it('should return user object if user is loggedin', function (done){
+
+    var demoProfile = profile(3, 'Testowy Profil', "Profil", "Test", "Testowy");
+
+    appUser.login(3, demoProfile, function (err, user){
+      if(err){ done(err); }
+      else{
+        // console.log(user);
+        assert.notStrictEqual(undefined, user._id);
+        assert.strictEqual(demoProfile.id, user.id);
+        assert.strictEqual(demoProfile.displayName, user.profile.displayName);
+        assert.strictEqual(demoProfile.name.familyName, user.profile.name.familyName);
+        assert.strictEqual(demoProfile.name.givenName, user.profile.name.givenName);
+        assert.strictEqual(demoProfile.name.middleName, user.profile.name.middleName);
+      
+        appUser.getLogin(3, function (err, user2){
+          if(err){ done(err); }
+          else{
+            assert.strictEqual(user._id.toString(), user2._id.toString());
+            assert.strictEqual(user.id, user2.id);
+            assert.strictEqual(user.profile.displayName, user2.profile.displayName);
+            assert.strictEqual(user.profile.name.familyName, user2.profile.name.familyName);
+            assert.strictEqual(user.profile.name.givenName, user2.profile.name.givenName);
+            assert.strictEqual(user.profile.name.middleName, user2.profile.name.middleName);
+            done();
+          }
+        });
+      }
+    });
+
+  });
+
+  it('should return null if user is notloggedin', function (done){
+    appUser.getLogin(0, function (err, user){
+      assert.strictEqual(null, user);
+      done();
+    });
+
+  });
+});
+
+describe('User.removeAccount', function (){
+  it('should remove user account if user exists and is loggedin', function (done){
+
+    var demoProfile = profile(4, 'Testowy Profil', "Profil", "Test", "Testowy");
+
+    appUser.login(4, demoProfile, function (err, user) {
+      if(err){ done(err) ;}
+      else{
+        // console.log(user);
+        assert.notStrictEqual(undefined, user._id);
+        assert.strictEqual(demoProfile.id, user.id);
+        assert.strictEqual(demoProfile.displayName, user.profile.displayName);
+        assert.strictEqual(demoProfile.name.familyName, user.profile.name.familyName);
+        assert.strictEqual(demoProfile.name.givenName, user.profile.name.givenName);
+        assert.strictEqual(demoProfile.name.middleName, user.profile.name.middleName);
+        
+
+        appUser.removeAccount(4, function (err, result){
+          if(err){ done(err); }
+          else{
+            assert.strictEqual(1, result);
+            Manager.findUser(user.id, function (err, user2){
+              if(err){ done(err); }
+              assert.strictEqual(null, user2);
+              done();
+            });
+          }
+        });
+      }
+    });
+
+  });
+
+  it('should not remove user account if user is not loggedin', function (done){
+
+    var demoProfile = profile(5, 'Testowy Profil', "Profil", "Test", "Testowy");
+
+    appUser.login(5, demoProfile, function (err, user){
+      if(err){ done(err); }
+      else{
+        // console.log(user);
+        assert.notStrictEqual(undefined, user._id);
+        assert.strictEqual(demoProfile.id, user.id);
+        assert.strictEqual(demoProfile.displayName, user.profile.displayName);
+        assert.strictEqual(demoProfile.name.familyName, user.profile.name.familyName);
+        assert.strictEqual(demoProfile.name.givenName, user.profile.name.givenName);
+        assert.strictEqual(demoProfile.name.middleName, user.profile.name.middleName);
+      
+        var fbid = appUser.logout(5);
+
+        assert.strictEqual(demoProfile.id, fbid);
+        
+        appUser.removeAccount(5, function (err, result){
+          if(err){ done(err); }
+          else{
+            assert.strictEqual(null, result);
+            done();
+          }
+        });
+      }
+    });
 
   });
 });
