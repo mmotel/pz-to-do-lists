06fd69d4812c95d0ca47e92d6040390a4a86a0ff
Flover <emflover@gmail.com>
2013-12-19 14:06:12 +0100

    added test for dataManager lib findAllGroups method

--- test/groupManagerTests.js
+++ test/groupManagerTests.js
@@ -90,3 +90,67 @@ describe('Manager.removeGroup', function (){
 		});
 	});
 });
+
+describe('Manager.findAllGroups', function (){
+	it('should find group', function (done){
+		var newGroup = new Group(2, 'Moja grupa', 'Opis mojej grupy');
+		var newGroup2 = new Group(2, 'Moja grupa 2', 'Opis mojej grupy 2');
+		Manager.createGroup(newGroup, function (err, item){
+			if (err){ done(err); }
+			else{
+				assert.notStrictEqual(undefined, item._id);
+				assert.notStrictEqual(undefined, item.id);
+				assert.strictEqual(newGroup.owner, item.owner);
+				assert.strictEqual(newGroup.name, item.name);
+				assert.strictEqual(newGroup.descr, item.descr);
+				assert.strictEqual(newGroup.owner, item.members[0].fbid);
+
+				Manager.createGroup(newGroup2, function (err, item2){
+					if (err){ done(err); }
+					else{
+						assert.notStrictEqual(undefined, item2._id);
+						assert.notStrictEqual(undefined, item2.id);
+						assert.strictEqual(newGroup2.owner, item2.owner);
+						assert.strictEqual(newGroup2.name, item2.name);
+						assert.strictEqual(newGroup2.descr, item2.descr);
+						assert.strictEqual(newGroup2.owner, item2.members[0].fbid);
+					
+						Manager.findAllGroups(2, function (err, items){
+							if(err){ done(err); }
+							else{
+								items.sort(function (a, b){
+									if (a.id < b.id){
+										return -1;
+									}
+									else if(a.id === b.id){
+										return 0; 
+									}
+									else {
+										return 1;
+									}
+								});
+								
+								assert.strictEqual(items[0]._id.toString(), item._id.toString());
+								assert.strictEqual(items[0].id, item.id);
+								assert.strictEqual(items[0].owner, item.owner);
+								assert.strictEqual(items[0].name, item.name);
+								assert.strictEqual(items[0].descr, item.descr);
+								assert.strictEqual(items[0].trash, item.trash);
+
+								assert.strictEqual(items[1]._id.toString(), item2._id.toString());
+								assert.strictEqual(items[1].id, item2.id);
+								assert.strictEqual(items[1].owner, item2.owner);
+								assert.strictEqual(items[1].name, item2.name);
+								assert.strictEqual(items[1].descr, item2.descr);
+								assert.strictEqual(items[1].trash, item2.trash);
+								done();
+
+							}
+						});
+					}
+				});
+			}
+		});
+	});
+});
+
