1faeb4aff38b204fb5ca47fb383006f3adac67e8
Flover <emflover@gmail.com>
2013-12-19 13:19:46 +0100

    added test for dataManager lib findGroup method

--- test/groupManagerTests.js
+++ test/groupManagerTests.js
@@ -27,3 +27,32 @@ describe('Manager.addGroup', function (){
 		});
 	});
 });
+
+describe('Manager.findGroup', function (){
+	it('should find group', function (done){
+		var newGroup = new Group(1, 'Moja grupa', 'Opis mojej grupy');
+		Manager.createGroup(newGroup, function (err, item){
+			if (err){ done(err); }
+			else{
+				assert.notStrictEqual(undefined, item._id);
+				assert.notStrictEqual(undefined, item.id);
+				assert.strictEqual(newGroup.owner, item.owner);
+				assert.strictEqual(newGroup.name, item.name);
+				assert.strictEqual(newGroup.descr, item.descr);
+				
+				Manager.findGroup(item.id, function (err, item2){
+					if (err){ done(err); }
+					else{
+						assert.strictEqual(item._id.toString(), item2._id.toString());
+						assert.strictEqual(item.id, item2.id);
+						assert.strictEqual(item.fbid, item2.fbid);
+						assert.strictEqual(item.name, item2.name);
+						assert.strictEqual(item.descr, item2.descr);
+						assert.strictEqual(item.trash, item2.trash);
+						done();
+					}
+				});
+			}
+		});
+	});
+});
