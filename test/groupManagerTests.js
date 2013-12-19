524c2c387073a8bd10f98a0b064a21a8e81cfb71
Flover <emflover@gmail.com>
2013-12-19 14:27:39 +0100

    added test for dataManager lib updateGroup method

--- test/groupManagerTests.js
+++ test/groupManagerTests.js
@@ -154,3 +154,39 @@ describe('Manager.findAllGroups', function (){
 	});
 });
 
+describe('Manager.updateGroup', function (){
+	it('should update group', function (done){
+		var newGroup = new Group(1, 'Moja grupa', 'Opis mojej grupy');
+		Manager.createGroup(newGroup, function (err, item){
+			if (err){ done(err); }
+			else{
+				assert.notStrictEqual(undefined, item._id);
+				assert.notStrictEqual(undefined, item.id);
+				assert.strictEqual(newGroup.owner, item.owner);
+				assert.strictEqual(newGroup.name, item.name);
+				assert.strictEqual(newGroup.descr, item.descr);
+				assert.strictEqual(newGroup.owner, item.members[0].fbid);
+				
+				var updatedData = {"id": item.id, "name": 'Moja Grupa Update', "descr": 'Opis mojej grupy update'};
+				Manager.updateGroup(updatedData, function (err, result){
+					if (err){ done(err); }
+					else{
+						assert.strictEqual(1, result);
+
+						Manager.findGroup(item.id, function (err, item2){
+							if (err){ done(err); }
+							else{
+								assert.strictEqual(item._id.toString(), item2._id.toString());
+								assert.strictEqual(item.id, item2.id);
+								assert.strictEqual(updatedData.name, item2.name);
+								assert.strictEqual(updatedData.descr, item2.descr);
+								done();
+							}
+						});
+					}
+				});
+			}
+		});
+	});
+});
+
